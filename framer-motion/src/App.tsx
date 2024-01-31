ae56fb45fa3319c26d5266b8d72ac9b7b71efae7
Ajaydeep123 <rajputajaydeepsingh@gmail.com>
2024-01-31 16:41:23 +0530

    Making any element a motion component

--- framer-motion/src/App.tsx
+++ framer-motion/src/App.tsx
@@ -1,9 +1,17 @@
 import {motion } from 'framer-motion';
+import { forwardRef } from 'react';
+
+const Button = forwardRef((props, ref)=>{
+  return <button ref={ref}>...</button>
+})
+
+const MotionButton = motion(Button);
+//now we want to make the Button Element a MOTION component as well, So, we forward all the properties of motion to the element ^-^
 
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      <motion.button>Click me!</motion.button>
     </h1>
   )
 }
